bd080b0508846eb1b0e59d6600a5469688b165b2
Julio Rodriguez <jircdev@gmail.com>
2022-11-03 09:55:16 -0300

    UI interface bug after remove waves component fixed. UI/UX bundle form modal.

--- lib/inspect/tests/files/modules/app/test-53/widget/ts/controller.ts
+++
@@ -1,9 +0,0 @@
-import {PageReactWidgetController} from '@beyond-js/react-widgets/page';
-import {View} from "./views";
-
-export /*bundle*/
-class Controller extends PageReactWidgetController {
-    get Widget() {
-        return View;
-    }
-}
