95cadf56367e9be67dbe52f2330afeb477725adc
henrybox <ebox@socites.com>
2022-11-12 14:19:11 -0300

    Developing new packaging method

--- applications/browser/dependencies/source.js
+++ applications/browser/dependencies/source.js
@@ -1,3 +1,14 @@
-module.exports = `
+module.exports = `(() => {
 const dependencies = new Map(/* dependencies */);
-`.trim();
+System.constructor.prototype.resolve = (id, parent) => {
+    let [resource, qs] = id.split('?');
+    qs = qs ? \`?\${qs}\` : \'\';
+    const split = resource.split('/');
+    const pkg = split[0].startsWith(\'@\') ? \`\${split.shift()}/\${split.shift()}\` : split.shift();
+    const subpath = split.length ? \`/\${split.join('/')}\` : '';
+
+    if(!dependencies.has(pkg)) return id;
+    const version = dependencies.get(pkg);
+    return \`http://localhost:8080/\${pkg}@\${version}\`;
+};
+})()`.trim();
