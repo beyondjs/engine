9cd264ebc048fff35efa178baa938f1db3aec499
henrybox <ebox@socites.com>
2022-11-22 12:53:55 -0300

    Developing new packaging method

--- plugins/native/processors/sass/css.js
+++ plugins/native/processors/sass/css.js
@@ -1,8 +1,9 @@
-const {ProcessorCode, Diagnostics, ProcessorStylesOutput} = require('beyond/plugins/sdk');
+const {ProcessorCode, Diagnostics, Diagnostic, ProcessorStylesOutput, SourceMap} = require('beyond/plugins/sdk');
+const sass = require('sass');
 
 module.exports = class extends ProcessorCode {
     get resource() {
-        return 'js';
+        return 'css';
     }
 
     get hash() {
@@ -13,11 +14,40 @@ module.exports = class extends ProcessorCode {
         super(...params);
     }
 
-    async _build(request) {
-        void request;
+    _prepared(require) {
+        const {files} = this.processor.sources;
+        require(files) && files.forEach(file => require(file));
+    }
 
+    _compileFile(file) {
+        let result;
+        try {
+            const options = {sourceMap: true};
+            result = sass.compileString(file.content, options);
+        }
+        catch (exc) {
+            const errors = [{message: exc.message}];
+            return {errors};
+        }
+
+        const {css, sourceMap: map} = result;
+        const code = css.toString();
+        map.sources[0] = file.relative.file;
+        return {code, map};
+    }
+
+    _build() {
+        const sourcemap = new SourceMap();
         const diagnostics = new Diagnostics();
-        const styles = new ProcessorStylesOutput({code: `#body: {color: 'blue'}`});
+
+        const {files} = this.processor.sources;
+        files.forEach(file => {
+            const {code, map, errors} = this._compileFile(file);
+            sourcemap.concat(code, null, map);
+        });
+
+        const {code, map} = sourcemap;
+        const styles = new ProcessorStylesOutput({code, map});
         return {diagnostics, styles};
     }
 }
