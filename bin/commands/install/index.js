db91839385e268dba9717307989f08094fcfc5bd
henrybox <ebox@socites.com>
2022-12-21 12:58:34 -0300

    Consume utils from @beyond-js/...

--- bin/commands/install/index.js
+++ bin/commands/install/index.js
@@ -3,6 +3,11 @@ const Installer = require('@beyond-js/uimport/installer');
 module.exports = {
     command: 'install',
     description: 'Installs the dependencies of the package',
+    options: [{
+        name: 'name',
+        required: true,
+        type: 'string'
+    }],
     handler: async () => {
         const installer = new Installer();
         await installer.process();
