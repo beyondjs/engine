3cbdb8a7c0fc72f815cd036eef9f74a73c3abf50
henrybox <ebox@socites.com>
2022-12-31 13:34:05 -0300

    New package/project dependencies method configuration updated

--- install-dependencies/index.js
+++ install-dependencies/index.js
@@ -1,11 +1,13 @@
 const {Config} = require('@beyond-js/config');
 const uimport = require('@beyond-js/uimport');
 
+const specs = {pkg: '@beyond-playground/counter-rendering@1.0.0'};
+
 (async () => {
     uimport.initialise();
     const Installer = require('@beyond-js/uimport/installer');
 
-    const internals = await (async () => {
+    const {json, internals} = await (async () => {
         const config = new Config(process.cwd(), {'/applications': 'array'});
         config.data = 'beyond.json';
 
@@ -16,6 +18,7 @@ const uimport = require('@beyond-js/uimport');
         applications.items.forEach(application => promises.push(application.ready));
         await Promise.all(promises);
 
+        let json;
         const internals = new Map();
         applications.items.forEach(application => {
             if (!application.valid) return;
@@ -25,12 +28,22 @@ const uimport = require('@beyond-js/uimport');
 
             const {dependencies, devDependencies, peerDependencies, version} = config;
             const name = config.scope ? `@${config.scope}/${config.name}` : config.name;
+            const vname = `${name}@${version}`;
 
-            internals.set(name, {version, dependencies, devDependencies, peerDependencies});
+            vname === specs.pkg && (json = Object.assign({name}, config));
+            internals.set(vname, {version, dependencies, devDependencies, peerDependencies});
         });
-        return internals;
+
+        config.destroy();
+
+        return {json, internals};
     })();
 
-    const installer = new Installer({internals});
+    if (!json) {
+        console.log(`Package "${specs.pkg}" not found`);
+        return;
+    }
+
+    const installer = new Installer({json, internals});
     await installer.process();
 })().catch(exc => console.log(exc.stack));
